88c3fa931254ddc62b0de7ce48a720fe01516933
mustadev <58852293+mustadev@users.noreply.github.com>
2020-08-23 19:16:08 +0100

    articleController implemented

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -7,10 +7,35 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    //
-
+    // get all articles
     public function index()
     {
         return Article::all();
     }
+
+    // show article by id
+    public function show($id){
+        return Article::find($id);
+    }
+
+    // store article
+    public function store(Request $request){
+        return Article::create($request->all());
+    }
+
+    // update article
+    public function update(Request $request, $id){
+        $article = Article::findOrFail($id);
+        $article->update($request->all());
+        return $article;
+    }
+
+    public function delete(Request $request, $id){
+        $article = Article::findOrFail();
+        $article->delete();
+
+        return 204;
+    }
+
+
 }
