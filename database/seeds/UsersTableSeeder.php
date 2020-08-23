569b78ac3a6ad0a15c61c13b22b708c9fb058806
mustadev <58852293+mustadev@users.noreply.github.com>
2020-08-23 18:34:32 +0100

    user seed implemented

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,6 +13,32 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // remove old records
+        User::truncate();
+
+        // instantiate faker
+        $faker = \Faker\Factory::create();
+
+        //create a hashed password
+        $password = Hash::make('password');
+
+        //create admin
+
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => $password
+        ]);
+        // add records
+
+        // And now let's generate a few dozen users for our app:
+        for ($i = 0; $i < 10; $i++) {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => $password,
+            ]);
+        }
+
     }
 }
