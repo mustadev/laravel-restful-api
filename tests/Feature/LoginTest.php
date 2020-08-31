8bf738e94313c21d949f96d6281216bdf0fe23f3
mustadev <58852293+mustadev@users.noreply.github.com>
2020-08-31 20:40:31 +0100

    testRequiresEmailAndLogin implemented

---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    public function testRequiresEmailAndLogin()
+    {
+        $this->json('POST', 'api/login')
+            ->assertStatus(422)
+            ->assertJson([
+                "message" => "The given data was invalid.",
+                "errors" => [
+                    "email" => [
+                        "The email field is required."
+                    ],
+                    "password" => [
+                        "The password field is required."
+                    ]
+                ]
+            ]);
+    }
+}
